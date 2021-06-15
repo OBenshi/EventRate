587c7a48bd1769e35a438010eee9a718fca9f350
OBenshi <65823304+OBenshi@users.noreply.github.com>
2021-06-15 17:34:14 +0200

    navigation in progress

--- client/src/views/ListView.js
+++ client/src/views/ListView.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { Grid } from "@material-ui/core";
+import PartyInfoCard from "../components/PartyInfoCard";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  useHistory,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
+function ListView(props) {
+  const [parties, setParties] = useState([]);
 
-function ListView() {
-  return <div>listView</div>;
+  useEffect(() => {
+    fetch("http://localhost:5000/parties/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setParties(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>listView</h1>
+      <Grid container align="center" spacing={3}>
+        {parties.length &&
+          parties.map((party) => (
+            <Grid item xs={12} key={party._id}>
+              {" "}
+              <PartyInfoCard party={party} />
+            </Grid>
+          ))}
+      </Grid>{" "}
+    </div>
+  );
 }
 
 export default ListView;
